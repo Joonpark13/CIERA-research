5607e2eeaf1027830debfe2e2c13fb65799ac039
Joon Park <JoonPark2017@u.northwestern.edu>
2016-10-03 01:54:38 -0500

    Implement binary neutron star count script.

--- scripts/neutron_stars.py
+++ scripts/neutron_stars.py
@@ -12,12 +12,13 @@ import matplotlib.pyplot as plt
 import bisect
 
 __author__ = "Joon Park"
-__version__ = 0.1
+__version__ = 1.0
 __email__ = "JoonPark@u.northwestern.edu"
 
 # Set data source directory relative to this file.
 DATA_DIR = os.path.join(os.path.dirname(__file__), "..", "N5K_r26_Z002_1", "save01")
-# The first 3 lines of each file are header information.
+# Star type
+NEUTRON_STAR = 13
 
 def get_single_data():
     """Returns the number of neutron stars with physical times.
@@ -39,6 +40,7 @@ def get_single_data():
     starcounts = [] # Will hold numbers of neutron stars at each timestep.
     for filename in glob.glob(single_stars):
         with open(filename, 'r') as f:
+            # Physical time is the second item in the first line
             physical_time = float(f.readline().split()[1])
             # Save position so the starcount can be inserted at the same position
             position = bisect.bisect(timesteps, physical_time)
@@ -53,19 +55,69 @@ def get_single_data():
             counter = 0
             for line in f:
                 data_line = line.split()
-                if int(data_line[1]) == 13:
+                # second item in row is star type
+                if int(data_line[1]) == NEUTRON_STAR:
                     counter += 1
             starcounts.insert(position, counter)
                     
     return (timesteps, starcounts)
 
+def get_binary_data():
+    """Returns the number of neutron stars with physical times.
+
+    For binary stars only, not single stars!
+
+    return format:
+        (
+            [physical_time_1, physical_time_2, ...],
+            [#_of_neutron_stars_1, #_of_neutron_stars_2, ...]
+        )
+
+        Ordered according to physical time.
+    """
+    # Grab all the binary star data files
+    binary_stars = os.path.join(DATA_DIR, "bev.82_*")
+
+    timesteps = [] # Will hold the physical times from each file
+    starcounts = [] # Will hold numbers of neutron stars at each timestep.
+    for filename in glob.glob(binary_stars):
+        with open(filename, 'r') as f:
+            # Physical time is the second item in the first line
+            physical_time = float(f.readline().split()[1])
+            # Save position so the starcount can be inserted at the same position
+            position = bisect.bisect(timesteps, physical_time)
+            # Insert so that timesteps is always ordered
+            bisect.insort(timesteps, physical_time)
+
+            # Count the number of neutron stars in this file (timestep)
+            counter = 0
+            for line in f:
+                data_line = line.split()
+                # third item in row is first star type
+                if int(data_line[2]) == NEUTRON_STAR:
+                    counter += 1
+                # fourth item in row is second star type
+                if int(data_line[3]) == NEUTRON_STAR:
+                    counter += 1
+            starcounts.insert(position, counter)
+
+    return (timesteps, starcounts)
+
 def main():
-    data = get_single_data()
-    plt.plot(data[0], data[1])
+    single_data = get_single_data()
+    plt.plot(single_data[0], single_data[1])
     plt.title("Evolution of Single Neutron Star Counts")
     plt.xlabel("Physical Time (Myr)")
     plt.ylabel("Neutron Star Count")
     plt.savefig("single_counts.png")
+    plt.clf()
+
+    binary_data = get_binary_data()
+    plt.plot(binary_data[0], binary_data[1])
+    plt.title("Evolution of Binary Neutron Star Counts")
+    plt.xlabel("Physical Time (Myr)")
+    plt.ylabel("Neutron Star Count")
+    plt.savefig("binary_counts.png")
 
 if __name__ == "__main__":
     main()
